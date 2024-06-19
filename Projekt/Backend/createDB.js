1913f65cdb8158121d76a180e92ea209aeb28bb7
MoserPatrick <moser-patrick@web.de>
2024-06-19 23:09:39 +0200

    Verbesserung

--- Projekt/Backend/createDB.js
+++ Projekt/Backend/createDB.js
@@ -7,12 +7,12 @@ db.serialize(() => {
     db.run('CREATE TABLE IF NOT EXISTS vorhanden (Frühlingsrollen INT, Frühlingsecken INT, Wantan INT, Muslitos INT, PhadThai INT, Tagesessen INT)');
   
     // Insert some sample data
-    const stmt = db.prepare('INSERT INTO users VALUES (?, ?)');
-    stmt.run(1,0,0,0,0,0);
+    const stmt = db.prepare('INSERT INTO vorhanden VALUES (?, ?, ?, ?, ?, ?)');
+    stmt.run(0,0,0,0,0,0);
     stmt.finalize();
   
     // Retrieve data and print it
-    db.each('SELECT * FROM users', (err, row) => {
+    db.each('SELECT * FROM vorhanden', (err, row) => {
       console.log(`${row.id} - ${row.name}`);
     });
   });
